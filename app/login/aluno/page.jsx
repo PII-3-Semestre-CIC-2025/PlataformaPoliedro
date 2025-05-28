38299df9a59696c6daaf5ddec0a51d2f20f8a14f
Leonardo T. Belo <160529755+LeonardoTBelo@users.noreply.github.com>
2025-05-28 15:04:26 -0300

    login aluno

---
+++ app/login/aluno/page.jsx
@@ -0,0 +1,74 @@
+'use client'
+import { useState } from 'react';
+import { ModalCadastro } from '@/app/components/modal-cadastro';
+import { supabase } from '@/lib/supabaseClient'
+import { useRouter } from 'next/navigation'
+
+
+const LoginAlunoPage = () => {
+    const [showCadastroModal, setShowCadastroModal] = useState(false);
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [erro, setErro] = useState(null)
+    const router = useRouter()
+
+    return(
+        <>
+        <div className="split-container">
+            <section className="login-box white-section">
+                <h2>Login</h2>
+                <form className="login-form"
+                    onSubmit={async (event) => {
+                        event.preventDefault()
+                        setErro(null)
+
+                        const { error } = await supabase.auth.signInWithPassword({
+                        email,
+                        password: senha,
+                        })
+
+                        if (error) {
+                        setErro(error.message)
+                        } else {
+                        router.push('/etapa')
+                        }
+                    }}>
+                    {erro && <p style={{ color: 'red' }}>{erro}</p>}
+                    <div className="form-group">
+                        <label>E-mail:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Senha:</label>
+                        <input
+                            type="password"
+                            id="senha"
+                            name="senha"
+                            required
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}
+                        />
+                    </div>
+
+                    <button type="submit" className="submit-button">Entrar</button>
+                </form>
+                <a href="#" className="forgot-password">Esqueceu a senha?</a>
+                
+            </section>
+
+            <div className="pattern-section d-none d-lg-block col-lg-6"  />
+        </div>
+
+        </>
+    );
+}
+
+export default LoginAlunoPage;
